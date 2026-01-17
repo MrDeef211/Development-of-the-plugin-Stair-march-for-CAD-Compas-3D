1c04af64fabdd1af98edeabc900b541d3446e797
MrDeef <aleksanrovich211@mail.ru>
2026-01-18 03:05:27 +0700

    переход на NUnits

--- stairway/Tests/ParametersUnitTests.cs
+++ stairway/Tests/ParametersUnitTests.cs
@@ -1,223 +1,176 @@
-using System.Reflection.Metadata;
 using Model;
+using NUnit.Framework;
 
 namespace Tests
 {
-    public class ParametersUnitTests
-    {
-        /// <summary>
-        /// —оздать класс параметров
-        /// </summary>
-        /// <returns>—сылка на созданный класс</returns>
-        private Parameters Create()
-        {
-            return new Parameters();
-        }
+	[TestFixture]
+	public class ParametersUnitTests
+	{
+		private Parameters Create()
+		{
+			return new Parameters();
+		}
 
-        /// <summary>
-        /// ѕроверка инициализации параметрa
-        /// </summary>
-        [Fact]
-        public void CreateOutOfRangeParameter()
-        {
+		[Test]
+		[Description("ѕроверка инициализации параметра вне допустимого диапазона")]
+		public void CreateOutOfRangeParameter()
+		{
+			Assert.Throws<Exception>(() =>
+				new Parameter(ParametersTypes.Height, 8000, 500, 32000));
 
-            Assert.Throws<Exception>(() =>
-            {
-                var parameter = new Model.Parameter(ParametersTypes.Height, 8000, 500, 32000);
-            });
+			Assert.Throws<Exception>(() =>
+				new Parameter(ParametersTypes.Height, 8000, 500, 0));
+		}
 
-            Assert.Throws<Exception>(() =>
-            {
-                var parameter = new Model.Parameter(ParametersTypes.Height, 8000, 500, 0);
-            });
-        }
+		[Test]
+		[Description("ѕроверка инициализации словар€ параметров")]
+		public void ConstructorInitializesAllParameters()
+		{
+			var parameters = Create();
+			var dict = parameters.GetParameters();
 
-        /// <summary>
-        /// ѕроверка инициализации словар€ параметров
-        /// </summary>
-        [Fact]
-        public void ConstructorInitializesAllParameters()
-        {
-            var parameters = Create();
+			Assert.That(dict, Is.Not.Null);
+			Assert.That(dict.Count, Is.GreaterThan(0));
+			Assert.That(dict.Keys, Does.Contain(ParametersTypes.Height));
+		}
 
-            var dict = parameters.GetParameters();
+		[TestCase(ParametersTypes.Height, 3200)]
+		[TestCase(ParametersTypes.Length, 3200)]
+		[TestCase(ParametersTypes.StepProjectionLength, 20)]
+		[TestCase(ParametersTypes.StepAmount, 20)]
+		[TestCase(ParametersTypes.StepHeight, 160)]
+		[TestCase(ParametersTypes.Width, 1000)]
+		[Description("ѕроверка передачи параметра")]
+		public void SetParameterChangesValue(ParametersTypes parameter, double value)
+		{
+			var parameters = Create();
 
-            Assert.NotNull(dict);
-            Assert.True(dict.Count > 0);
-            Assert.Contains(ParametersTypes.Height, dict.Keys);
-        }
+			parameters.SetParameter(parameter, value);
+			double currentValue = parameters.GetParameter(parameter);
 
-        /// <summary>
-        /// ѕроверка передачи параметра
-        /// </summary>
-        [Theory]
-        [InlineData(ParametersTypes.Height, 3200)]
-        [InlineData(ParametersTypes.Length, 3200)]
-        [InlineData(ParametersTypes.StepProjectionLength, 20)]
-        [InlineData(ParametersTypes.StepAmount, 20)]
-        [InlineData(ParametersTypes.StepHeight, 160)]
-        [InlineData(ParametersTypes.Width, 1000)]
-        public void SetParameterChangesValue(ParametersTypes parameter, double value)
-        {
-            var parameters = Create();
+			Assert.That(currentValue, Is.EqualTo(value));
+		}
 
-            parameters.SetParameter(parameter, value);
+		[Test]
+		[Description("ѕроверка передачи параметра вне диапазона")]
+		public void SetParameterOutOfRangeRaisesErrorEvent()
+		{
+			var parameters = Create();
+			ErrorArgs? error = null;
 
-            double currentValue = parameters.GetParameter(parameter);
+			parameters.ErrorMessageEvent += (s, e) => error = e;
 
-            Assert.Equal(value, currentValue);
-        }
+			parameters.SetParameter(ParametersTypes.Length, 500);
+			parameters.SetParameter(ParametersTypes.StepAmount, 2);
+			parameters.SetParameter(ParametersTypes.Height, 400);
 
-        /// <summary>
-        /// ѕроверка передачи неправильного параметра
-        /// </summary>
-        [Fact]
-        public void SetParameterOutOfRangeRaisesErrorEvent()
-        {
-            var parameters = Create();
-            ErrorArgs? error = null;
+			Assert.That(error, Is.Not.Null);
+			Assert.That(error.ParametersList, Does.Contain(ParametersTypes.Height));
+		}
 
-            parameters.ErrorMessageEvent += (s, e) =>
-            {
-                error = e;
-            };
+		[Test]
+		[Description("ѕроверка передачи неправильного количества ступеней")]
+		public void SetStepAmountOutOfRangeRaisesErrorEvent()
+		{
+			var parameters = Create();
+			ErrorArgs? error = null;
 
-            parameters.SetParameter(ParametersTypes.Length, 500);
-            parameters.SetParameter(ParametersTypes.StepAmount, 2);
-            parameters.SetParameter(ParametersTypes.Height, 400); // меньше Min
+			parameters.ErrorMessageEvent += (s, e) => error = e;
 
-            Assert.NotNull(error);
-            Assert.Contains(ParametersTypes.Height, error.ParametersList);
-            Assert.Equal(": параметр не должен выходить за диапазон от " +
-                            parameters.GetParameters()[ParametersTypes.Height].Min.ToString() +
-                            " до " + parameters.GetParameters()[ParametersTypes.Height].Max.ToString() + " мм", error.Message);
-        }
+			parameters.SetParameter(ParametersTypes.StepAmount, 100);
 
-        /// <summary>
-        /// ѕроверка передачи неправильного параметра количества ступеней
-        /// </summary>
-        [Fact]
-        public void SetStepAmountOutOfRangeRaisesErrorEvent()
-        {
-            var parameters = Create();
-            ErrorArgs? error = null;
+			Assert.That(error, Is.Not.Null);
+			Assert.That(error.ParametersList, Does.Contain(ParametersTypes.StepAmount));
+		}
 
-            parameters.ErrorMessageEvent += (s, e) =>
-            {
-                error = e;
-            };
+		[Test]
+		[Description("ѕроверка передачи неправильного выступа ступени")]
+		public void SetStepProjectionOutOfRangeRaisesErrorEvent()
+		{
+			var parameters = Create();
+			ErrorArgs? error = null;
 
-            parameters.SetParameter(ParametersTypes.StepAmount , 100);
+			parameters.ErrorMessageEvent += (s, e) => error = e;
 
-            Assert.NotNull(error);
-            Assert.Contains(ParametersTypes.StepAmount, error.ParametersList);
-        }
+			parameters.SetParameter(ParametersTypes.StepProjectionHeight, 90);
 
-        /// <summary>
-        /// ѕроверка передачи неправильных параметров выступа
-        /// </summary>
-        [Fact]
-        public void SetStepProjectionOutOfRangeRaisesErrorEvent()
-        {
-            var parameters = Create();
-            ErrorArgs? error = null;
+			Assert.That(error, Is.Not.Null);
+			Assert.That(error.ParametersList, Does.Contain(ParametersTypes.StepProjectionHeight));
+		}
 
-            parameters.ErrorMessageEvent += (s, e) =>
-            {
-                error = e;
-            };
-            parameters.SetParameter(ParametersTypes.StepProjectionHeight, 90); 
+		[Test]
+		[Description("ѕроверка передачи нецелочисленного количества ступеней")]
+		public void StepAmountNotIntegerRaisesError()
+		{
+			var parameters = Create();
+			ErrorArgs? error = null;
 
-            Assert.NotNull(error);
-            Assert.Contains(ParametersTypes.StepProjectionHeight, error.ParametersList);
-        }
+			parameters.ErrorMessageEvent += (s, e) => error = e;
 
+			parameters.SetParameter(ParametersTypes.StepAmount, 5.5);
 
-        /// <summary>
-        /// ѕроверка передачи не целочисленного параметра в "количество ступеней"
-        /// </summary>
-        [Fact]
-        public void StepAmountNotIntegerRaisesError()
-        {
-            var parameters = Create();
-            ErrorArgs? error = null;
+			Assert.That(error, Is.Not.Null);
+			Assert.That(error.ParametersList, Does.Contain(ParametersTypes.StepAmount));
+		}
 
-            parameters.ErrorMessageEvent += (s, e) => error = e;
+		[Test]
+		[Description("ѕроверка пересчета высоты ступени при изменении высоты")]
+		public void ChangingHeightRecalculatesStepHeight()
+		{
+			var parameters = Create();
 
-            parameters.SetParameter(ParametersTypes.StepAmount, 5.5);
+			parameters.SetParameter(ParametersTypes.StepAmount, 10);
+			parameters.SetParameter(ParametersTypes.Height, 2000);
 
-            Assert.NotNull(error);
-            Assert.Contains(ParametersTypes.StepAmount, error.ParametersList);
-        }
+			double stepHeight = parameters.GetParameter(ParametersTypes.StepHeight);
 
-        /// <summary>
-        /// ѕроверка  внутреннего вычислени€ высоты ступени
-        /// </summary>
-        [Fact]
-        public void ChangingHeightRecalculatesStepHeight()
-        {
-            var parameters = Create();
+			Assert.That(stepHeight, Is.EqualTo(200));
+		}
 
-            parameters.SetParameter(ParametersTypes.StepAmount, 10);
-            parameters.SetParameter(ParametersTypes.Height, 2000);
+		[Test]
+		[Description("ѕроверка пересчета высоты марша при изменении высоты ступени")]
+		public void ChangingStepHeightRecalculatesHeight()
+		{
+			var parameters = Create();
 
-            double stepHeight = parameters.GetParameter(ParametersTypes.StepHeight);
+			parameters.SetParameter(ParametersTypes.StepAmount, 10);
+			parameters.SetParameter(ParametersTypes.StepHeight, 180);
 
-            Assert.Equal(200, stepHeight);
-        }
+			double height = parameters.GetParameter(ParametersTypes.Height);
 
-        /// <summary>
-        /// ѕроверка внутреннего вычислени€ высоты марша
-        /// </summary>
-        [Fact]
-        public void ChangingStepHeightRecalculatesHeight()
-        {
-            var parameters = Create();
+			Assert.That(height, Is.EqualTo(1800));
+		}
 
-            parameters.SetParameter(ParametersTypes.StepAmount, 10);
-            parameters.SetParameter(ParametersTypes.StepHeight, 180);
+		[Test]
+		[Description("ѕроверка валидации угла марша")]
+		public void StairAngleOutOfRangeRaisesError()
+		{
+			var parameters = new Parameters();
+			var errors = new List<ErrorArgs>();
 
-            double height = parameters.GetParameter(ParametersTypes.Height);
+			parameters.ErrorMessageEvent += (s, e) => errors.Add(e);
 
-            Assert.Equal(1800, height);
-        }
+			parameters.SetParameter(ParametersTypes.Height, 8000);
+			parameters.SetParameter(ParametersTypes.Length, 1000);
 
-        /// <summary>
-        /// ѕроверка внутренней валидации угла марша
-        /// </summary>
-        [Fact]
-        public void StairAngleOutOfRangeRaisesError()
-        {
-            var parameters = new Parameters();
-            var errors = new List<ErrorArgs>();
+			Assert.That(errors, Has.Some.Matches<ErrorArgs>(e =>
+				e.ParametersList.Contains(ParametersTypes.Height) &&
+				e.ParametersList.Contains(ParametersTypes.Length)));
+		}
 
-            parameters.ErrorMessageEvent += (s, e) => errors.Add(e);
+		[Test]
+		[Description("ѕроверка обновлени€ всех параметров")]
+		public void FullUpdateParametersSendsAllParameters()
+		{
+			var parameters = Create();
+			List<ParametersTypes>? updated = null;
 
-            parameters.SetParameter(ParametersTypes.Height, 8000);
-            parameters.SetParameter(ParametersTypes.Length, 1000);
+			parameters.UpdateParametersEvent += (s, list) => updated = list;
 
-            Assert.Contains(errors, e =>
-                e.ParametersList.Contains(ParametersTypes.Height) &&
-                e.ParametersList.Contains(ParametersTypes.Length));
-        }
+			parameters.FullUpdateParameters();
 
-        /// <summary>
-        /// ѕроверка обновлени€ всех параметров
-        /// </summary>
-        [Fact]
-        public void FullUpdateParametersSendsAllParameters()
-        {
-            var parameters = Create();
-            List<ParametersTypes>? updated = null;
-
-            parameters.UpdateParametersEvent += (s, list) => updated = list;
-
-            parameters.FullUpdateParameters();
-
-            Assert.NotNull(updated);
-            Assert.Equal(parameters.GetParameters().Count, updated.Count);
-        }
-
-
-    }
+			Assert.That(updated, Is.Not.Null);
+			Assert.That(updated.Count, Is.EqualTo(parameters.GetParameters().Count));
+		}
+	}
 }
